c235d7085545cac32d5b9706a273c5df590c4c41
Thiago Vieira <thiago.a.vieira@hotmail.com>
2024-10-20 02:34:23 -0300

    fix imports

--- feegow-clinic-vaccination/vite.config.js
+++ feegow-clinic-vaccination/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
 
 export default defineConfig({
   plugins: [

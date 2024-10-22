2c3a8f74daf51056e5d77f2aec0aa20227cde6ad
Thiago Vieira <thiago.a.vieira@hotmail.com>
2024-10-21 23:02:31 -0300

    caching is back

--- feegow-clinic-vaccination/app/Listeners/FlushCacheListener.php
+++ feegow-clinic-vaccination/app/Listeners/FlushCacheListener.php
@@ -5,11 +5,12 @@ namespace App\Listeners;
 use App\Events\EmployeeChanged;
 use App\Events\ReportChanged;
 use App\Events\VaccineChanged;
+use Illuminate\Support\Facades\Cache;
 
 class FlushCacheListener
 {
     public function handle(EmployeeChanged | VaccineChanged | ReportChanged $event)
     {
-        // Cache::flush();
+        Cache::flush();
     }
 }
